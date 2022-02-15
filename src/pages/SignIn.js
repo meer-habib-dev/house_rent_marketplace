d1d2396117d9b4e9f3600d71ef1ba5df78d829eb
Meer habib <meerhabib@Meers-MacBook-Air.local>
2022-02-15 23:34:31 +0600

    init

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 const SignIn = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const { email, password } = formData;
+    const location = useLocation()
+    
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+        <>
+            <div className="pageContainer">
+                <header>
+                    <p className="pageHeader">
+                        
+                    </p>
+                </header>
+                <main>
+                    <form > 
+                        <input type="email" className='emailInput' placeholder='Email' id='email' value={email} onChange={onChange}/>
+                    </form>
+                </main>
+            </div>
+        </>
     );
 };
 
